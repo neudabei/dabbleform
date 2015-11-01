5110d39e08c33b97573fc83d3facca4ef0bfaabc
neudabei <rschultzejena@gmail.com>
2015-11-01 22:02:21 +0000

    flesh out controller, add dynamic route, install pry gem

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,21 @@
 class SubmissionsController < ApplicationController
+  def create
+    @submission = Submission.new(params[:submission])
+    # @request_domain = env["HTTP_REFERER"]
+    # email from route of form submission /email@domain.com 
+    # find account by email@domain.com
+    # account website  == env["HTTP_REFERER"] ?
+
+    if @submission.save
+      render plain "Thanks for submitting!"
+    else
+      render plain "Something went wrong!"
+    end
+  end
+  
+  private
+
+    def submission_params
+      params.require(:submission).permit(:email, :website, :message)
+    end
 end
