d3e68bd504d927c091dc1a33b0d8930f005f2d5d
neudabei <rschultzejena@gmail.com>
2015-11-22 21:20:09 +0000

    add extra path to decision tree in case account exists, but email doesn't belong to website of account

--- app/controllers/accounts_verification_controller.rb
+++ app/controllers/accounts_verification_controller.rb
@@ -1,13 +1,20 @@
 class AccountsVerificationController < ApplicationController
 
   def create
-    binding.pry
     # if params[:token] == @account.token
     # update column 'account.verified' to true
     # and render page "thanks for verifying your account. this website is now ready to accept submissions from your dabbleform"
     # else
     # render page 'your token has expired. try submitting again or get in touch with support via support@dabbleform.com'
     # end
+
+    account = Account.find_by(token: params[:token])
+    if account && account.token == params[:token]
+      account.update(verified: true)
+      render plain: "Thanks for verifying your account. The website #{account.website} is now ready to accept submissions from your dabbleform" 
+    else
+      render plain: "Your token has expired. Try submitting again or get in touch with support via support@dabbleform.com" 
+    end
   end
 
 end
--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -4,16 +4,23 @@ class SubmissionsController < ApplicationController
   def create
 
     if website_belongs_to_existing_account?
-      if @existing_account.verified?
-        @submission = Submission.new(email: params[:email], name: params[:name], message: params[:message], account_id: @existing_account.id)
-          if @submission.save
-            render plain: "Thanks for submitting!"
-          else
-            render plain: "Something went wrong!"
-          end
-        AppMailer.send_new_submission_email(@submission, @existing_account).deliver_now
+      if @existing_account.email == account_email_submitted_to
+        if @existing_account.verified?
+          @submission = Submission.new(email: params[:email], name: params[:name], message: params[:message], account_id: @existing_account.id)
+            if @submission.save
+              render plain: "Thanks for submitting!"
+            else
+              render plain: "Something went wrong!"
+            end
+          AppMailer.send_new_submission_email(@submission, @existing_account).deliver_now
+        else
+          render plain: "Thanks for submitting! Please check your email #{account_email_submitted_to} and verify this account."
+          AppMailer.send_account_verification_email(@existing_account).deliver_now
+        end
       else
         render plain: "Thanks for submitting! Please check your email #{account_email_submitted_to} and verify this account."
+        @existing_account.update(email: account_email_submitted_to, verified: false, token: generate_token)
+        @existing_account = Account.find_by(website: website_submitted_from) # reassign account to variable to include the new email address
         AppMailer.send_account_verification_email(@existing_account).deliver_now
       end
     else 
@@ -46,59 +53,5 @@ class SubmissionsController < ApplicationController
   def generate_token
     SecureRandom.urlsafe_base64
   end
-
-
-
-  # def create_old
-
-  #   account_email_submitted_to = params[:account_email] + "." + params[:format]
-
-  #   @account = Account.find_by(email: account_email_submitted_to) # email from domain not from form
-
-  #   def account_verified?(account)
-  #     if account
-  #       get_host_without_www(env["HTTP_REFERER"]).start_with?(account.website)
-  #     else
-  #       false
-  #     end
-  #     # +  account_email_submitted_to belongs to this account
-  #     # && account_email_submitted_to == account.email
-  #   end
-
-  #   # if account_verified? 
-  #     # create new entry with corresponding account_id
-  #     # send email to the account owner informing about new entry
-  #   # else
-  #     # send verification email to account_email_submitted_to
-  #     # add new account 
-  #   # end
-
-  #   binding.pry
-
-  #   if account_verified?(@account)
-  #     submission = Submission.new(email: params[:email], account_id: @account.id)
-  #     if submission.save
-  #       render plain: "Thanks for submitting!"
-  #       # redirect_to "submission domain"
-  #       # send email to the account owner informing about new entry
-  #     else
-  #       render plain: "Something went wrong!"
-  #     end
-  #   else
-  #     #render plain: "Account doesn't seem to be verified"
-  #     # add new account:
-  #       # - write domain created with get_host method to db as well as email submitted to
-  #     # send verification email to account_email_submitted_to
-
-      
-  #     new_account = Account.new(email: account_email_submitted_to, website: get_host_without_www(env["HTTP_REFERER"]), verified: false)
-  #     if new_account.save
-  #       render plain: "Thanks for submitting! Please check your email #{account_email_submitted_to} and verify this account."
-  #       AppMailer.send_new_account_verification_email(new_account).deliver_now
-  #     else
-  #       render plain: "Something went wrong!"
-  #     end
-  #   end
-  # end
 
 end
