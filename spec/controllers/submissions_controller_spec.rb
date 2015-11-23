9952ae386fb6afed85dd4bcc5793f2002f4eb8de
neudabei <rschultzejena@gmail.com>
2015-11-23 12:18:33 +0000

    submission_controller test 'website from existing account belongs to email submitted do'

--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -5,12 +5,35 @@ describe SubmissionsController do
     context "website submitted from belongs to existing account" do
       context "website from existing account belongs to email submitted do" do
         context "existing account is verified" do
-          it "adds submission to existing account"
-          it "sends email to account owner with new submission content"
+          before do
+            @account = Account.create(email: 'email@domain.com', website: 'domain.com', verified: true, token: "1a2b3c")
+            @request.env["HTTP_REFERER"] = "http://www.domain.com"
+            post :create, {email: 'john@email.com', name: 'John', message: 'Hey there, great website!', account_email: 'email@domain', format: 'com'}
+          end
+
+          it "adds submission to existing account" do
+            expect(Submission.count).to eq(1)
+          end
+
+          it "sends email to account owner informing about new submission" do
+            expect(ActionMailer::Base.deliveries.last.to).to eq(["email@domain.com"])
+          end
         end
+        
         context "existing account is not verified" do
-          it "sends out email with verification link"
-          it "shows page informing about necessity to confirm account via link in email"
+          before do
+            Account.create(email: 'email@domain.com', website: 'domain.com', verified: false, token: "1a2b3c")
+            @request.env["HTTP_REFERER"] = "http://www.domain.com"
+            post :create, {email: 'john@email.com', name: 'John', message: 'Hey there, great website!', account_email: 'email@domain', format: 'com'}
+          end
+
+          it "sends out email with verification link" do
+            expect(ActionMailer::Base.deliveries.last.to).to eq(["email@domain.com"])
+          end
+
+          it "shows page informing about necessity to confirm account via link in email" do
+            expect(response.body).to eq("Thanks for submitting! Please check your email email@domain.com and verify this dabbleform for your website.") 
+          end
         end
       end
 
@@ -20,7 +43,7 @@ describe SubmissionsController do
           @request.env["HTTP_REFERER"] = "http://www.domain.com"
           post :create, {email: 'john@email.com', name: 'John', message: 'Hey there, great website!', account_email: 'new_email@domain', format: 'com'}
         end
-        
+
         it "adds email to account" do
           expect(@account.reload.email).to eq("new_email@domain.com")
         end
