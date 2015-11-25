83a94425aedc0563e2cb9dcaad9649d6880cac27
neudabei <rschultzejena@gmail.com>
2015-11-25 10:52:36 +0000

    fix associations between models/tables

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,3 @@
 class Account < ActiveRecord::Base
   has_many :websites
-  validates :website, uniqueness: true
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -1,4 +1,5 @@
 class Website < ActiveRecord::Base
   belongs_to :account
   has_many :submissions
+  validates :domain, uniqueness: true
 end
