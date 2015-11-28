1fe1375cf191468509f03c13723eca9a03d412d2
neudabei <rschultzejena@gmail.com>
2015-11-28 08:55:09 +0000

    clean up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  def require_verification
-    # if not verified send out email to the address submitted to /email
-  end
-
-  def submission_id
-    Account.find_by(email: params[:email]).id
-  end
 
   # method from: http://stackoverflow.com/questions/6674230/how-would-you-parse-a-url-in-ruby-to-get-the-main-domain
   # Only parses twice if url doesn't start with a scheme
