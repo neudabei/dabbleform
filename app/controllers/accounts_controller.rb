0d75448d79ef4f3ca981b6bf2186eb2e97c53eb7
neudabei <rschultzejena@gmail.com>
2015-11-28 08:55:38 +0000

    clean up AccountController

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,13 +1,2 @@
 class AccountsController < ApplicationController
-
-  # def create
-  #   binding.pry
-  #   @submission_params
-  #   account = Account.new()
-  #   if save
-  #     #send email
-  #   else
-
-  #   end
-  # end
 end
